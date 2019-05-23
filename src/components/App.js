ca775466752eb76447e83516fa7469ea9b70c485
admir.cejvanovic <admir.cejvanovic@m2bit.com>
2019-05-23 12:47:22 +0200

    Batman change.

--- src/components/App.js
+++ src/components/App.js
@@ -15,7 +15,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            I am Batman.
           </a>
         </header>
       </div>
